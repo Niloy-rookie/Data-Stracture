ed34e622614705e6bbc19e50606425f3ea3a91ad
Niloy <s2410476110@ru.ac.bd>
2025-11-05 18:06:44 +0600

    Created push random function

---
+++ LinkList/DLL.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int value;
+    Node* next;
+    Node* previous;
+
+    Node(int value) {
+        this->value = value;
+        this->next = NULL;
+        this->previous = NULL;
+    }
+};
+
+class DLL {
+    Node* head;
+    Node* tail;
+
+public:
+    DLL(){
+        head = tail = NULL;
+    }
+    int size = 0;
+    void push_front(int value) {
+        Node* node = new Node(value);
+        if(head == NULL){
+            head = tail = node;
+            size++;
+            return;
+        }
+        node->next = head;
+        head->previous = node;
+        head = node;
+        size++;
+    }
+    void push_last(int value) {
+        Node* node = new Node(value);
+        node->previous = tail;
+        tail->next = node;
+        tail = node;
+        size++;
+    }
+    void push(int value,int index) {
+        if(head == NULL){
+            push_front(value);
+            size++;
+            return;
+        }
+        Node*temp = head;
+        Node* node = new Node(value);
+        for(int i = 0;i < index-1;i++){
+            temp = temp->next;
+        }
+        if(temp == tail){
+            push_last(value);
+            return;
+        }
+        node->next = temp->next;//connection node to the adress of temp holding means next node of temp
+        node->previous = temp;//connection:node to temp
+        temp->next->previous = node;//connection:the node after newly node to newlynode
+        temp->next = node;//connection:node before newlynode to newlynode
+        size++;
+    }
+    void print() {
+        Node* temp = head;
+        while(temp != NULL){
+            cout << temp->value << "->";
+            temp = temp->next;
+        }
+        cout << "End" << endl;
+    }
+};
+
+int main(){
+    DLL dl;
+    dl.push_front(1);
+    dl.push_front(2);
+    dl.push_front(3);
+    dl.push_front(4);
+    dl.push(10,2);
+    dl.print();
+}
